2ddf81319aef338e878ca1d4c54b30106c46df8f
ProffessorLOGAN <82794943+ProffessorLOGAN@users.noreply.github.com>
2024-06-03 17:00:27 +0530

    user router added

---
+++ src/User/userRouter.ts
@@ -0,0 +1,8 @@
+import express from "express";
+
+const userRouter = express.Router();
+//routes
+userRouter.post("/register", (req, res) => {
+  res.json({ message: "user registered" });
+});
+export default userRouter;
