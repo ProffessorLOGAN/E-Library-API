1a8355db9ae9e2ead1c36c53c4986859eb571cbe
ProffessorLOGAN <82794943+ProffessorLOGAN@users.noreply.github.com>
2024-06-03 20:55:41 +0530

    Signin JWT token ,password hash added

--- src/User/userController.ts
+++ src/User/userController.ts
@@ -1,6 +1,9 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
+import bcrypt from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { config } from "../config/config";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
@@ -12,17 +15,31 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
   }
 
   // Database Call
-  const user = await userModel.findOne({email});
+  const user = await userModel.findOne({ email });
 
-  if(user){
-    const error = createHttpError(400,"User already exists with this email");
+  if (user) {
+    const error = createHttpError(400, "User already exists with this email");
     return next(error);
   }
 
+  /// password -> hash
+  const hashPassword = await bcrypt.hash(password, 10);
+
   //Process / Logic
+  const newUser = await userModel.create({
+    name,
+    email,
+    password: hashPassword,
+  });
+
+  //Token Generation JWT
+
+  const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
+    expiresIn: "7d",
+  });
 
   // Response
-  res.json({ message: "User created" });
+  res.json({ accessToken: token });
 };
 
 export { createUser };
