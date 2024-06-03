3157a5b9a0b4d5b27dfc9f8219a9215072900e1d
ProffessorLOGAN <82794943+ProffessorLOGAN@users.noreply.github.com>
2024-06-03 17:22:42 +0530

    add user Validation

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import globalErrorHandler from "./middlewares/globalErrorHander";
+import userRouter from "./User/userRouter";
 
 const app = express();
+
+app.use(express.json());
 
 // Routes
 app.get("/", (req, res, next) => {
   res.json({ message: "Welcome to Elibrary API" });
 });
+
+app.use("/api/users", userRouter);
 
 //Global Error Handler
 app.use(globalErrorHandler);
